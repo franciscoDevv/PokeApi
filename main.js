695f3e00486d4ae3f3ac07077a61435ca956ba3a
Tux <77551844+Pancho-Developer@users.noreply.github.com>
2021-11-05 12:25:04 -0300

    New features

--- main.js
+++ main.js
@@ -1,23 +1,33 @@
 const API_URL = 'https://pokeapi.co/api/v2/pokemon/';
 const sendnum = document.getElementById('button');
-const num = document.getElementById('num').value;
-
+ 
 button.addEventListener('click', () => {
-	const pokemonId = document.getElementById("num").value;
-	fetch(`${API_URL}${pokemonId}`)
-	.then(response => response.json())
-	.then(data => {
-		const app = document.getElementById('app');
-		app.innerHTML = `
-		<div id="atributos">
-		<div>Nombre: ${data.name} <br> Altura: ${data.height * 10} cm <br> Peso: ${data.weight} kg</div>
-		<div><img src="${data.sprites.front_default}" height="150"></div>
-		</div>
-		`;
-		document.getElementById('num').value = "";
-})
-})
+        const num = document.getElementById('num').value;
 
+        if (num.length == 0 || num .value <= 0 || num.value >= 899) {
+            
+            app.innerHTML = `<p style="text-align: center">Ingrese un numero válido por favor 👾</p>`
+        }
 
+        else{
+
+    const pokemonId = document.getElementById("num").value;
+
+    fetch(`${API_URL}${pokemonId}`)
+    .then(response => response.json())
+    
+    .then(data => {
+        const app = document.getElementById('app');
+        
+        app.innerHTML = `
+        <div id="atributos">
+        <div>Nombre: ${data.name} <br> Altura: ${data.height * 10} cm <br> Peso: ${data.weight} kg</div>
+        <div><img src="${data.sprites.front_default}" height="150"></div>
+        </div>
+        `;
+        document.getElementById('num').value = "";
+        
+})}
+})
 
 
